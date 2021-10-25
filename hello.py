5d288e12510f0bb8fc47a2796d6848fe89f8cc01
Frappy <44483724+Frappyy@users.noreply.github.com>
2021-10-25 15:52:00 +0300

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 while(True):
-    kroonid = input("Sisesta kroonid: ")
+    eek = input("Sisesta kroonid: ")
 
-    eur = float(kroonid) * 0.06
+    eur = float(eek) * 0.06
 
     print("Teisendatud summa on: ", round(eur))
