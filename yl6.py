fe1fbecd4b398fb1a310791ada48494c19a03cae
Fredy Ait <aitfredy@gmail.com>
2021-10-27 15:54:09 +0300

    commit

--- yl6.py
+++ yl6.py
@@ -2,3 +2,9 @@ x = input("Sisesta esimene arv (x): ")
 y = input("Sisesta teine arv (y): ")
 z = input("Sisesta kolmas arv (z): ")
 
+if x > y and x > z:
+    print("Suurem number on X:", x)
+elif y > x and y > z:
+    print("Suurem number on Y:", y)
+elif z > x and z > y:
+    print("Suurem number on Z:", z)
