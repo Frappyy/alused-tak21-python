6dafa8b1905c4343f742ef0321b280082cedf0ab
Fredy Ait <aitfredy@gmail.com>
2021-11-08 09:04:08 +0200

    commit

---
+++ yl10.py
@@ -0,0 +1,17 @@
+name = input("Mis su nimi on?: ")
+
+print("Tere", name)
+
+location = input("Kus sa elad?: ")
+
+if location == "Saaremaa":
+    print("Tere saarlane!")
+
+age = int(input("Kui vana sa oled?: "))
+
+if age < 18:
+    print("Sa oled liiga noor, et autot juhtida.")
+elif age == 18:
+    print("Õnnitlused täisealiseks saamisel!")
+elif age > 18:
+    print("Sa oled piisavalt vana, et autoga sõita.")
