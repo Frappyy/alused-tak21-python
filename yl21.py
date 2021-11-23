92f29d3de91c36310fe8cc7d550b0ceef87d2cef
Fredy Ait <aitfredy@gmail.com>
2021-11-23 10:55:20 +0200

    init commit

---
+++ yl21.py
@@ -0,0 +1,15 @@
+import random
+
+rNum = random.randrange(0, 100)
+
+while(True):
+    print(rNum)
+    uInput = int(input("Arva ära mis numbri see valis: "))
+
+    if uInput == rNum:
+        print("Valisid õigesti!")
+        break
+    elif uInput > rNum:
+        print("Vihje: sinu arvatud number on suurem kui arvuti oma.")
+    else:
+        print("Vihje: sinu arvatud number on väiksem kui arvuti oma.")
